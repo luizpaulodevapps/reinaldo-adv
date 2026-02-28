c69469f9fdbe83c3c6122b47d381b0c2e526eb89
Luiz Paulo dev <luizpaulo.dev.apps@gmail.com>
2026-02-28 17:04:08 +0000

    d

--- src/app/(dashboard)/page.tsx
+++
@@ -1,8 +0,0 @@
-/**
- * ARQUIVO NEUTRALIZADO RGMJ
- * Este arquivo foi desativado para evitar conflitos de rota com a Landing Page principal.
- * A porta de entrada do Dashboard é /dashboard.
- */
-export default function InactivePage() {
-  return null;
-}
